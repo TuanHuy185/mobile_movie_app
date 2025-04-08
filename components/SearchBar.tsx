9206f780594592e90a98712a2bd8855c4dce1d12
TuanHuy185 <huy.lamtuan1805@hcmut.edu.vn>
2025-04-08 22:35:20 +0700

    feat(fe): add api from TMDB to get movie

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,16 +1,28 @@
-import { TextInput, View, Image } from 'react-native';
-import React from 'react';
-import { icons } from '@/constants/icons';
+import { TextInput, View, Image } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
 
-const SearchBar = () => {
+interface Props {
+  placeholader: string;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ placeholader, onPress }: Props) => {
   return (
     <View className="flex-row items-center bg-drak-200 rounded-full px-5 py-4">
-      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
       <TextInput
-        onPress={() => {}}
-        placeholder="Search"
-        value=''
+        onPress={onPress}
+        placeholder={placeholader}
+        value=""
         onChangeText={() => {}}
+        placeholderTextColor={"#a8b5db"}
+        className="flex-1 ml-2 text-white"
       />
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -1,54 +1,36 @@
 export const TMDB_CONFIG = {
-  BASE_URL: "https://api.themoviedb.org/3",
+  BASE_URL: 'https://api.themoviedb.org/3',
   API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
-    accept: "application/json",
+    accept: 'application/json',
     Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
-};
+}
 
-export const fetchMovies = async ({
-  query,
-}: {
-  query: string;
-}): Promise<Movie[]> => {
-  const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+export const fetchPopularMovies = async ({query}:{query:string}) => {
+  const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
   const response = await fetch(endpoint, {
-    method: "GET",
+    method: 'GET',
     headers: TMDB_CONFIG.headers,
   });
-
   if (!response.ok) {
     throw new Error(`Failed to fetch movies: ${response.statusText}`);
   }
-
   const data = await response.json();
   return data.results;
-};
+}
 
-export const fetchMovieDetails = async (
-  movieId: string
-): Promise<MovieDetails> => {
-  try {
-    const response = await fetch(
-      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
-      {
-        method: "GET",
-        headers: TMDB_CONFIG.headers,
-      }
-    );
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTg4ZjU4ODBmMTk5YTI1YTE4ODM4ZmY3MmM0ZjU1YSIsIm5iZiI6MTc0NDEyMDYwNi44MTIsInN1YiI6IjY3ZjUyYjFlYTU0NzFhNTFlZTk5OTI1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fEMGnW8WV_dRc_Rwtwc2uIZBJ-HsrbSQ0sMjyPjLzB0'
+//   }
+// };
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch movie details: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching movie details:", error);
-    throw error;
-  }
-};
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
